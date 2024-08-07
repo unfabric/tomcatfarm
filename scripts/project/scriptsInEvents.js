369a948957c291877a3594447ea22fde66cd106c
unfabric <bigteknoloji@gmail.com>
2024-08-07 10:56:15 +0300

    catfarm

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async GameEvent_Event4_Act7(runtime, localVars)
+	{
+		window.addEventListener('beforeunload', (event) =>  { runtime.callFunction('save')});
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
